ac0228c32485702c2107459a2db54879a5b5b813
Anat Flashner <anatflashner@gmail.com>
2025-09-15 16:08:27 +0300

    client: adjust entry files (index.html, manifest.json, main.jsx)

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/global.css'
 import App from './App'
-import { DuckProvider } from './context/DuckContext'
+import { MemoriesProvider } from './context/memoryContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <DuckProvider>
+    <MemoriesProvider>
       <App />
-    </DuckProvider>
+    </MemoriesProvider>
   </React.StrictMode>
 )
