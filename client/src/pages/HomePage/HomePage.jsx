174c7a4264521f59cdb0c34a72fd1e7a54464921
Anat Flashner <anatflashner@gmail.com>
2025-09-15 16:29:08 +0300

    client(ui): update HomePage layout and styles

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import styles from './Home.module.css';
-import RandomDuck from '../../components/RandomDuck/RandomDuck';
+import ItemsGrid from '../../components/MemoryCard/ItemsGrid';
 
 
 const Home = () => {
   return (
     <div className={styles.home}>
-      <h1 className={styles.headline}>Duck It</h1>
-      <RandomDuck />
+      <h1 className={styles.headline}>Memories</h1>
+      <h2 className={styles.secondheadline}>little sparks of joy</h2>
+      <ItemsGrid />
     </div>
   );
 };
